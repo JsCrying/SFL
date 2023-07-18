727e48d4008e63763469d83ff4218cd2e5bcc9f7
JsCrying <413797366@qq.com>
2023-07-18 20:19:33 +0800

    上传卫星轨道仿真文件

---
+++ orbit/main.m
@@ -0,0 +1,28 @@
+day_list = [120, 180];
+for day = day_list
+    startTime = datetime(2023, 6, 1, 00, 00, 0);
+    stopTime = startTime + days(day);
+    sampleTime = 60; % seconds
+    sc = satelliteScenario(startTime, stopTime, sampleTime);
+    
+    % shanghai 经纬度
+    lat = 31.2304;
+    lon = 121.4737;
+    gs = groundStation(sc, lat, lon);
+    
+    height = 550e3; % m
+    R_earth = 6.37814e+06; % m
+    radius = height + R_earth; % m
+    inc = 53;
+    total_sat = 20;
+    total_plan = 5;
+    F = 1;
+    RAAN = 10;
+    sat = walkerDelta(sc, radius, inc, total_sat, total_plan, F, RAAN=RAAN);
+    
+    ac = access(sat, gs);
+    intvls = accessIntervals(ac);
+    intvls = sortrows(intvls, 5);
+    file_name = [num2str(total_sat), 'SA_' , num2str(day), 'DAY', '_endtime', '.csv'];
+    writetable(intvls, file_name);
+end
